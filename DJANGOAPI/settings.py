ba2ee641a1d5c94913b0d84c9be35d7060334b54
loveyueliang <yueliangxiaxiangyu@163.com>
2019-06-25 17:53:25 +0800

    取消sqlite连接

--- DJANGOAPI/settings.py
+++ DJANGOAPI/settings.py
@@ -84,8 +84,9 @@ WSGI_APPLICATION = 'DJANGOAPI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        "ENGINE": None,
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
